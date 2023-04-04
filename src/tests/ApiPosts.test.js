a8f48ebb79222ac4356e3c92a00a878b7e945646
Anson Yan <ansonyan@seas.upenn.edu>
2023-04-04 18:04:59 -0400

    fixed apiposts test

--- src/tests/ApiPosts.test.js
+++ src/tests/ApiPosts.test.js
@@ -1,20 +1,44 @@
 /* eslint-disable no-undef */
 // import { test, expect } from '@testing-library/jest-dom';
-import { getAllPosts, getPostById } from '../api/posts';
+import axios from 'axios';
+import {
+  createNewClassroom,
+} from '../api/posts';
 
-test('getAllPosts', async () => {
-  const data = await getAllPosts();
-  expect(data[0].title).toBe('hw3 q5 help');
-  expect(data[0].body).toBe('help');
-  expect(data[0].private).toBe(true);
-  expect(data[0].anonymous).toBe(false);
-  expect(data.length).toBe(10);
+jest.mock('axios');
+
+afterEach(() => {
+  jest.clearAllMocks();
 });
 
-test('getPostById', async () => {
-  const data = await getPostById(2);
-  expect(data.title).toBe('hw1 q7 help');
-  expect(data.body).toBe('help');
-  expect(data.private).toBe(true);
-  expect(data.anonymous).toBe(false);
+test('createNewClassroom create a new classroom', async () => {
+  const classroomObject = {
+    name: 'Cis 350',
+    admins: ['1'],
+    users: ['1', '2'],
+    posts: [],
+  };
+  const newClassroom = { ...classroomObject, id: 3 };
+  axios.post.mockResolvedValueOnce({ data: newClassroom });
+
+  const result = await createNewClassroom(classroomObject);
+
+  expect(axios.post).toHaveBeenCalledWith('/api/classrooms', classroomObject);
+  expect(result).toEqual(newClassroom);
+});
+
+test('createNewClassroom return an error if API call fails', async () => {
+  const classroomObject = {
+    name: 'Cis 350',
+    admins: ['1'],
+    users: ['1', '2'],
+    posts: [],
+  };
+  const error = new Error('Network Error');
+  axios.post.mockRejectedValueOnce(error);
+
+  const result = await createNewClassroom(classroomObject);
+
+  expect(axios.post).toHaveBeenCalledWith('/api/classrooms', classroomObject);
+  expect(result).toEqual(error);
 });
